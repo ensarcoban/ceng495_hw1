6b4ea1e0c71b1bf252c688dcd3f1504322bb8f6b
ensarcoban <eyupenssr@gmail.com>
2022-04-05 15:18:45 +0300

    Almost the last version.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+feather.replace({ 'aria-hidden': 'true' })
+
 document.querySelectorAll('#schedule_table td').forEach(function (element) {
     element.addEventListener('click', function (event) {
         if (event.target.style.backgroundColor === 'red') {
@@ -76,20 +78,41 @@ document.getElementById('add_seat_geek_event').addEventListener('click', async f
 
 // Export functions
 function createPDF() {
-    var sTable = document.getElementById('schedule_table').innerHTML;
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    // source can be HTML-formatted string, or a reference
+    // to an actual DOM element from which the text will be scraped.
+    source = document.getElementById('schedule_table')[0];
 
-    // CREATE A WINDOW OBJECT.
-    var win = window.open('', '', 'height=700,width=700');
-
-    win.document.write('<html><head>');
-    win.document.write('<title>Profile</title>');   // <title> FOR PDF HEADER.
-    win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
-    win.document.write('</head>');
-    win.document.write('<body>');
-    win.document.write(sTable);         // THE TABLE CONTENTS INSIDE THE BODY TAG.
-    win.document.write('</body></html>');
-
-    win.document.close(); 	// CLOSE THE CURRENT WINDOW.
-
-    win.print();    // PRINT THE CONTENTS.
+    // we support special element handlers. Register them with jQuery-style
+    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+    // There is no support for any other type of selectors
+    // (class, of compound) at this time.
+    specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '#bypassme': function(element, renderer) {
+            // true = "handled elsewhere, bypass text extraction"
+            return true
+        }
+    };
+    margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522
+    };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+            source, // HTML string or DOM elem ref.
+            margins.left, // x coord
+            margins.top, {// y coord
+                'width': margins.width, // max width of content on PDF
+                'elementHandlers': specialElementHandlers
+            },
+    function(dispose) {
+        // dispose: object with X, Y of the last line add to the PDF
+        //          this allow the insertion of new lines after html
+        pdf.save('Test.pdf');
+    }
+    , margins);
 }
