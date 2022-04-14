e7a3ab3cf6cd378aeb27a935815d8b99a6315bb0
ensarcoban <eyupenssr@gmail.com>
2022-04-14 21:57:08 +0300

    file export added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import pdfkit
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -6,6 +7,18 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
+
+
+@app.post('/export')
+def exports():
+    print(request.data.decode())
+    pdfkit.from_string(request.data.decode(), 'static/img/output.pdf')
+    return request.data
+
+
+@app.route('/export')
+def get_export():
+    return render_template('export-page.html')
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+// import html2canvas from 'html2canvas';
+
 feather.replace({ 'aria-hidden': 'true' })
 
 document.querySelectorAll('#schedule_table td').forEach(function (element) {
@@ -76,43 +78,19 @@ document.getElementById('add_seat_geek_event').addEventListener('click', async f
 
 // console.log(document.getElementById('11').innerText == '');
 
-// Export functions
-function createPDF() {
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    // source can be HTML-formatted string, or a reference
-    // to an actual DOM element from which the text will be scraped.
-    source = document.getElementById('schedule_table')[0];
-
-    // we support special element handlers. Register them with jQuery-style
-    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-    // There is no support for any other type of selectors
-    // (class, of compound) at this time.
-    specialElementHandlers = {
-        // element with id of "bypass" - jQuery style selector
-        '#bypassme': function(element, renderer) {
-            // true = "handled elsewhere, bypass text extraction"
-            return true
-        }
-    };
-    margins = {
-        top: 80,
-        bottom: 60,
-        left: 40,
-        width: 522
-    };
-    // all coords and widths are in jsPDF instance's declared units
-    // 'inches' in this case
-    pdf.fromHTML(
-            source, // HTML string or DOM elem ref.
-            margins.left, // x coord
-            margins.top, {// y coord
-                'width': margins.width, // max width of content on PDF
-                'elementHandlers': specialElementHandlers
-            },
-    function(dispose) {
-        // dispose: object with X, Y of the last line add to the PDF
-        //          this allow the insertion of new lines after html
-        pdf.save('Test.pdf');
-    }
-    , margins);
-}
+// Export to PDF
+document.getElementById('export_button').addEventListener('click', function (event) {
+    console.log("selam");
+    // send a post request with fetch api
+    fetch('/export', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/HTML'
+        },
+        body: document.getElementById('schedule_table').outerHTML
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        });
+});
